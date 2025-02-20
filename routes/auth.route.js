b69d9014fd75020fa3f1f57ae358e971516c5ff3
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-20 17:21:19 +0100

    Name changed

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/auth.model");
+const User = require("../models/user.model");
 
 const router = express.Router();
 
