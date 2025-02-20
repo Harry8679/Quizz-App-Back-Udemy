e89b0853423d86b78dc38b30b1e6ff4e9bc9d047
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-20 16:45:43 +0100

    Quizz Model updated

---
+++ models/quiz.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const QuestionSchema = new mongoose.Schema({
+  question: { type: String, required: true },
+  options: [{ type: String, required: true }],
+  correctAnswer: { type: String, required: true },
+  category: { type: String, required: true },
+  difficulty: { type: String, enum: ["easy", "medium", "hard"], required: true }
+});
+
+module.exports = mongoose.model("Question", QuestionSchema);
