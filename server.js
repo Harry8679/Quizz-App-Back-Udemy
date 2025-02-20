b6f97de859a764fdfb8b2c3e6497a0328c517be8
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-20 16:41:26 +0100

    server.js updated

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connecté"))
+  .catch((err) => console.log("Erreur de connexion MongoDB :", err));
+
+app.use("/api/auth", require("./routes/auth.route"));
+app.use("/api/quiz", require("./routes/quiz.route"));
+
+const PORT = process.env.PORT || 4900;
+app.listen(PORT, () => console.log(`✅ Serveur lancé sur le port ${PORT}`));
