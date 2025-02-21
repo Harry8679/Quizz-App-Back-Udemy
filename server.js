a91b8a5d8940a20f363193d3ebd6d39bfe02b58c
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-21 09:21:05 +0100

    Database connected

--- server.js
+++ server.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connecté"))
+mongoose.connect(process.env.MONGO_URI, {}).then(() => console.log("MongoDB connecté"))
   .catch((err) => console.log("Erreur de connexion MongoDB :", err));
 
 // app.use("/api/auth", require("./routes/auth.route"));
