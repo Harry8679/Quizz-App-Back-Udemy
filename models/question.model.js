011f91e3f1daa34326b983c1cd0fb36b19309157
Harry MacCode <emarh.harry.code@gmail.com>
2025-02-22 16:43:30 +0100

    question model

--- models/question.model.js
+++ models/question.model.js
@@ -4,7 +4,7 @@ const QuestionSchema = new mongoose.Schema({
   question: { type: String, required: true },
   options: [{ type: String, required: true }],
   correctAnswer: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { type: String, enum: ["maths", "physique", "chimie", "informatique"], required: true },
   difficulty: { type: String, enum: ["easy", "medium", "hard"], required: true }
 });
 
